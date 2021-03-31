813acfd2a76390c67ae1fe8bef7bc913e575597a
MigajaSuarez <78988059+MigajaSuarez@users.noreply.github.com>
2021-03-31 06:15:08 -0500

    basic routing aded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoComponent } from './listado/listado.component';
+import { IntroComponent } from './intro/intro.component'
+import { BuscarComponent} from './buscar/buscar.component'
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'movies', component: ListadoComponent},
+  {path:'intro', component: IntroComponent},
+  {path:'buscar', component: BuscarComponent}
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
